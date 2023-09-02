3106d8446366bde35b09a5d4ce7bbab189668d04
joaopedrojhl <jhlhoaopedro@gmail.com>
2023-09-01 23:56:32 -0300

    primo

--- lista1/atividade2/Exercicio1.java
+++ lista1/atividade2/Exercicio1.java
@@ -1,5 +1,47 @@
 package lista1.atividade2;
 // CALCULADORA
+import java.util.Scanner;
+
 public class Exercicio1 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("CALCULADORA");
+        System.out.println("Digite o primeiro número da operação: ");
+        double numero1 =  scanner.nextDouble();
+
+        System.out.println("Digite o segundo número da operação: ");
+        double numero2 =  scanner.nextDouble();
+
+        System.out.println("Digite a operação (+, -, *, /): ");
+        char operacao = scanner.next().charAt(0);
+
+        double resultado = 0.0;
+
+        switch (operacao){
+            case '+':
+                resultado = numero1 + numero2;
+                break;
+            case '-':
+                resultado = numero1 - numero2;
+                break;
+            case '*':
+                resultado = numero1 * numero2;
+                break;
+            case '/':
+                if(numero2 != 0){
+                    resultado = numero1 / numero2;
+                }else{
+                    System.out.println("Erro: Divisão por zero não é permitida");
+                }
+                break;
+            default:
+                System.out.println("Operação inválida. Use +, -, * ou /");    
+        }
+
+        System.out.println("Resultado: " + resultado);
+
+        scanner.close();    }
     
 }
+
--- lista1/atividade2/Exercicio2.java
+++ lista1/atividade2/Exercicio2.java
@@ -1,5 +1,37 @@
 package lista1.atividade2;
 // CONVERSOR DE TEMPERATURA
+import java.util.Scanner;
+
 public class Exercicio2 {
-    
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("CONVERSOR DE TEMPERATURA");
+        System.out.println("Escolha para qual temperatura irá mudar: Celsius para Fahrenheit - 'C' ou Fahrenheit para Celsius - 'F'): ");
+        char direcao = scanner.next().charAt(0);
+
+        double temperaturaCelsius;
+        double temperaturaFahrenheit;
+
+        if (direcao == 'C' || direcao == 'c') {
+            System.out.println("Digite a temperatura em Celsius: ");
+            temperaturaCelsius = scanner.nextDouble();
+            temperaturaFahrenheit = celsiusParaFahrenheit(temperaturaCelsius);
+            System.out.println("Temperatura em Fahrenheit: " + temperaturaFahrenheit);
+        } else if (direcao == 'F' || direcao == 'f') {
+            System.out.println("Digite a temperatura em Fahrenheit: ");
+            temperaturaFahrenheit = scanner.nextDouble();
+            temperaturaCelsius = fahrenheitParaCelsius(temperaturaFahrenheit);
+            System.out.println("Temperatura em Celsius: " + temperaturaCelsius);
+        } else{
+            System.out.println("Conversão inválida. Use 'C' para Celsius para Fahrenheit ou 'F' para Fahrenheit para Celsius.");
+        }
+    }
+
+    public static double celsiusParaFahrenheit(double celsius){
+        return (celsius * 9/5) + 32;
+    }
+    public static double fahrenheitParaCelsius(double fahrenheit){
+        return (fahrenheit - 32) * 9/50;
+    }
 }
